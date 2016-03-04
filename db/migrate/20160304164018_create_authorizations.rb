b0ac81077fc718e46f9f72a43146feb57683c690
Rob Lacey <contact@robl.me>
2016-03-04 22:32:22 +0000

    table_name change

--- db/migrate/20160304164018_create_authorizations.rb
+++ db/migrate/20160304164018_create_authorizations.rb
@@ -1,10 +1,13 @@
 class CreateAuthorizations < ActiveRecord::Migration[5.0]
-  def change
-    create_table :authorizations do |t|
+  def up
+    create_table :infectious_authorizations do |t|
       t.string :provider
       t.string :access_token
       t.text :data
       t.timestamps null: false
     end
+  end
+  def down
+    drop_table :authorizations
   end
 end
