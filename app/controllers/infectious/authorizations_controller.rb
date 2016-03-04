1dfbf2565547c64133b3674e1b878938e7c3ca1e
Rob Lacey (braindeaf) <contact@robl.me>
2016-03-04 09:41:47 +0000

    Spotify callback test

--- app/controllers/infectious/authorizations_controller.rb
+++ app/controllers/infectious/authorizations_controller.rb
@@ -1,14 +1,19 @@
 module Infectious
   class AuthorizationsController < Infectious::ApplicationController
     def new
-      redirect_to "https://accounts.spotify.com/en/authorize?client_id=0f8a80290bf14a2d9d8371892916a677&redirect_uri=http:%2F%2Frobl.me%2Fspotify%2Fcallback&response_type=code"
+      redirect_to "https://accounts.spotify.com/en/authorize?client_id=0f8a80290bf14a2d9d8371892916a677&redirect_uri=#{callback_url}&response_type=code"
     end
 
     def callback
-      render text: params.to_yaml
+      body = JSON.parse(Faraday.new(url: 'https://accounts.spotify.com').post('/api/token', code: params[:code], redirect_uri: callback_url, grant_type: 'authorization_code').body)
+      render text: body.to_yaml
     end
 
     private
+
+    def callback_url
+      callback_spotify_url
+    end
 
     def service_module
       params[:type].camelize
