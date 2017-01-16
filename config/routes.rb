75e06a53ff6c8e8fe9cd32a4b59407332b37e9f1
Rob Lacey <contact@robl.me>
2017-01-16 20:00:53 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Infectious::Engine.routes.draw do
   %w(spotify facebook instagram).each do |type|
     get "/#{type}" => 'authorizations#new', defaults: { type: type }
     get "/#{type}/callback" => 'authorizations#callback', defaults: { type: type }
-    get "/#{type}/subscriptions/callback", 'subscriptions#callback', defaults: { type: type }
+    match "/#{type}/subscriptions/callback", 'subscriptions#callback', defaults: { type: type }, via: [:get, :post]
   end
 end
